134b8363b6b4cd96b00379efba609a14f14614fa
Nadav Alon <alonadav2000@gmail.com>
2026-02-05 15:33:14 +0200

    update num_samples to 1 so it doesnt rerun 10 times for nothing

--- run_bench_lucas_limits.sh
+++ run_bench_lucas_limits.sh
@@ -1,3 +1,3 @@
-./submit_samples.sh spot:ltlf lucas:mso lucas:belief-states --test-dir=lucas --level=part --limit=600
-./submit_samples.sh spot:ltlf lucas:mso lucas:belief-states --test-dir=lucas --level=0 --limit=600
-./submit_samples.sh spot:ltlf lucas:mso lucas:belief-states --test-dir=lucas --level=all --limit=600
+./submit_samples.sh spot:ltlf lucas:mso lucas:belief-states --test-dir=lucas --level=part --limit=600 --num-samples=1
+./submit_samples.sh spot:ltlf lucas:mso lucas:belief-states --test-dir=lucas --level=0 --limit=600 --num-samples=1
+./submit_samples.sh spot:ltlf lucas:mso lucas:belief-states --test-dir=lucas --level=all --limit=600 --num-samples=1
