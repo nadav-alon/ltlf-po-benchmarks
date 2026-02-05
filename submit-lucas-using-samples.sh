ad52e8bd3eb43d06ec7b8591ceb14d0008131505
Nadav Alon <alonadav2000@gmail.com>
2026-02-05 15:31:18 +0200

    switch scripts

--- submit-lucas-using-samples.sh
+++
@@ -1,3 +0,0 @@
-./submit_samples.sh --test-dir=lucas --level=part --limit=600
-./submit_samples.sh --test-dir=lucas --level=0 --limit=600
-./submit_samples.sh --test-dir=lucas --level=all --limit=600
